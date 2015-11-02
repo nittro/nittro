c0a5f02dda7cfa12defa2f0379e1dc7b24c31acb
danik <me@subsonic.cz>
2015-11-02 22:23:50 +0100

    fix line endings etc

--- src/js/Nette/loader.js
+++ src/js/Nette/loader.js
@@ -20,3 +20,4 @@
 /*! @include file="Widgets/Dialog.js" */
 /*! @include file="Widgets/Confirm.js" */
 /*! @include file="Widgets/FormDialog.js" */
+/*! @include file="Widgets/FlashMessages.js" */
---
+++ src/loader.js
@@ -0,0 +1,5 @@
+/* jahudka/minify compatible loader */
+/*! @include file="js/promise.js" */
+/*! @include file="js/context.js" */
+/*! @include file="js/Utils/loader.js" */
+/*! @include file="js/Nette/loader.js" */
