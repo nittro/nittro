3af8be87c65ead932630e58aa14cddbcf16199af
danik <me@subsonic.cz>
2016-02-11 21:42:15 +0100

    Some more cleanups in meta files

---
+++ loader.js
@@ -0,0 +1,5 @@
+/* jahudka/minify compatible loader */
+/*! @include file="src/js/promise.js" */
+/*! @include file="src/js/context.js" */
+/*! @include file="src/js/Utils/loader.js" */
+/*! @include file="src/js/Nette/loader.js" */
--- src/loader.js
+++
@@ -1,5 +0,0 @@
-/* jahudka/minify compatible loader */
-/*! @include file="js/promise.js" */
-/*! @include file="js/context.js" */
-/*! @include file="js/Utils/loader.js" */
-/*! @include file="js/Nette/loader.js" */
