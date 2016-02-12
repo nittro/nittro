565130e2c3a069dce720e87cb33973f5a3eebf70
danik <me@subsonic.cz>
2016-02-12 22:58:58 +0100

    replaced embedded promise implementation with a bower dependency

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,6 @@
 module.exports = function (grunt) {
 
     var NetteJS = [
-        'src/js/promise.js',
         'src/js/context.js',
         'src/js/Utils/Strings.js',
         'src/js/Utils/Arrays.js',
@@ -43,7 +42,22 @@ module.exports = function (grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        js: {
+
+        netteForms: {
+            fix: {
+                files: {
+                    'dist/js/netteForms.js': [
+                        'bower_components/nette-forms/src/assets/netteForms.js'
+                    ]
+                }
+            }
+        },
+
+        uglify: {
+            options: {
+                mangle: false,
+                sourceMap: false
+            },
             min: {
                 files: {
                     'dist/js/nette.min.js': NetteJS
@@ -51,8 +65,12 @@ module.exports = function (grunt) {
             },
             full: {
                 files: {
-                    'dist/js/nette.full.js': NetteJS.concat(
-                        'src/js/bootstrap.js'
+                    'dist/js/nette.full.js': [
+                        'bower_components/promiz/promiz.min.js'
+                    ].concat(
+                        NetteJS,
+                        'src/js/bootstrap.js',
+                        'src/js/stack.js'
                     )
                 }
             },
@@ -60,17 +78,14 @@ module.exports = function (grunt) {
                 files: {
                     'dist/js/stack.min.js': [
                         'src/js/stack.js'
-                    ]
-                }
-            },
-            'stack-async': {
-                files: {
+                    ],
                     'dist/js/stack-async.min.js': [
                         'src/js/stack-async.js'
                     ]
                 }
             }
         },
+
         less: {
             min: {
                 options: {
@@ -98,48 +113,10 @@ module.exports = function (grunt) {
         },
 
         jasmine: {
-            src: [
-                'src/js/promise.js',
-                'src/js/context.js',
-                'src/js/Utils/Strings.js',
-                'src/js/Utils/Arrays.js',
-                'src/js/Utils/HashMap.js',
-                'src/js/Utils/DateInterval.js',
-                'src/js/Utils/DateTime.js',
-                'src/js/Utils/Url.js',
-                'src/js/Utils/DOM.js',
-                'src/js/Utils/ReflectionClass.js',
-                'src/js/Utils/ReflectionFunction.js',
-                'src/js/Nette/EventEmitter.js',
-                'src/js/Nette/Freezable.js',
-                'src/js/Nette/Object.js',
-                'src/js/Nette/Utils/Tokenizer.js',
-                'src/js/Nette/Neon/Neon.js',
-                'src/js/Nette/Ajax/FormData.js',
-                'src/js/Nette/Ajax/Request.js',
-                'src/js/Nette/Ajax/Response.js',
-                'src/js/Nette/Ajax/Service.js',
-                'src/js/Nette/Ajax/Transport/Native.js',
-                'src/js/Nette/Page/Snippet.js',
-                'src/js/Nette/Page/Transitions.js',
-                'src/js/Nette/Page/Service.js',
-                'src/js/Nette/Forms/VendorCompiled.js',
-                'src/js/Nette/Forms/Form.js',
-                'src/js/Nette/Forms/Locator.js',
-                'src/js/Nette/DI/Container.js',
-                'src/js/Nette/DI/Context.js',
-                'src/js/Nette/Application/Storage.js',
-                'src/js/Nette/Application/Routing/URLRoute.js',
-                'src/js/Nette/Application/Routing/DOMRoute.js',
-                'src/js/Nette/Application/Routing/Router.js',
-                'src/js/Nette/Widgets/DialogBase.js',
-                'src/js/Nette/Widgets/Dialog.js',
-                'src/js/Nette/Widgets/Confirm.js',
-                'src/js/Nette/Widgets/FormDialog.js',
-                'src/js/Nette/Widgets/FlashMessages.js'
-            ],
+            src: NetteJS,
             options: {
                 vendor: [
+                    'bower_components/promiz/promiz.min.js',
                     'bower_components/nette-forms/src/assets/netteForms.js'
                 ],
                 specs: 'tests/specs/**.spec.js',
@@ -149,38 +126,21 @@ module.exports = function (grunt) {
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-jasmine');
-    grunt.registerTask('default', ['js', 'less']);
+    grunt.registerTask('default', ['netteForms', 'uglify', 'less']);
     grunt.registerTask('test', ['jasmine']);
 
 
-    grunt.registerMultiTask('js', 'Compile js', function () {
-        var UglifyJS = require('uglify-js'),
-            path = require('path');
-
-        var options = this.options({
-            compress: true
-        });
-
+    grunt.registerMultiTask('netteForms', 'Fix netteForms.js', function () {
         this.files.forEach(function (f) {
-            var source;
-
-            if (options.compress) {
-                source = UglifyJS.minify(f.src, {
-                    mangle: false,
-                    compress: {
-                        unused: false
-                    }
-                }).code;
-            } else {
-                source = f.src.map(function (src) {
-                    return grunt.file.read(src);
-                }).join("\n\n");
-            }
+            var source = f.src.map(grunt.file.read).join("\n");
+            source = source.replace(/^[ \t]*global\.Nette\.initOnLoad\(\);[ \t]*$/mg, '');
+            grunt.file.write(f.dest, source);
 
             grunt.file.write(f.dest, source);
-            grunt.log.ok('Compiled ' + path.basename(f.dest));
+            grunt.log.ok('Fixed ' + f.dest.replace(/^.+?([^\/]+)$/, '$1'));
 
         });
     });
